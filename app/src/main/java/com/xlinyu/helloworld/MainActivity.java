6bbc54941a1723103850e32461a005e8219c5844
虫虫 <ncc0706@gmail.com>
2016-06-07 22:21:53 +0800

    减少money并提示

--- app/src/main/java/com/xlinyu/helloworld/MainActivity.java
+++ app/src/main/java/com/xlinyu/helloworld/MainActivity.java
@@ -5,6 +5,7 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -14,6 +15,8 @@ public class MainActivity extends AppCompatActivity {
 
     private int money = 0;
 
+    private Button btnLoseMoney;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -22,14 +25,32 @@ public class MainActivity extends AppCompatActivity {
         btnGetMoney = (Button) findViewById(R.id.btnGetMoney);
         tvGetMoney = (TextView) findViewById(R.id.tvGetMoney);
 
-        btnGetMoney.setOnClickListener(new View.OnClickListener(){
+        btnLoseMoney = (Button) findViewById(R.id.btnLoseMoney);
+
+
+        btnGetMoney.setOnClickListener(new View.OnClickListener() {
 
             @Override
-            public void onClick(View v){
+            public void onClick(View v) {
                 money++;
                 tvGetMoney.setText("哈哈，我通过鼠标点击，轻易赚取了" + money + "元");
             }
         });
+
+        btnLoseMoney.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (money == 0) {
+                    //提示用户余额不足
+                    Toast.makeText(MainActivity.this, "现在已经是穷光蛋了，不要再点击了", Toast.LENGTH_SHORT).show();
+                } else {
+                    money--;
+                    tvGetMoney.setText("哈哈，我通过鼠标点击，轻易赚取了" + money + "元");
+
+                }
+            }
+        });
+
 
     }
 }
