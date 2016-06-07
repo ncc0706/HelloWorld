90ac0675501bf7a84235809f1d2a903a6db43493
虫虫 <ncc0706@gmail.com>
2016-06-07 22:57:49 +0800

    简单的样式

--- app/src/main/java/com/xlinyu/helloworld/MainActivity.java
+++ app/src/main/java/com/xlinyu/helloworld/MainActivity.java
@@ -4,6 +4,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -17,6 +18,8 @@ public class MainActivity extends AppCompatActivity {
 
     private Button btnLoseMoney;
 
+    private EditText etGoaldMoney;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -26,14 +29,23 @@ public class MainActivity extends AppCompatActivity {
         tvGetMoney = (TextView) findViewById(R.id.tvGetMoney);
 
         btnLoseMoney = (Button) findViewById(R.id.btnLoseMoney);
-
+        etGoaldMoney = (EditText) findViewById(R.id.etGoaldMoney);
 
         btnGetMoney.setOnClickListener(new View.OnClickListener() {
 
             @Override
             public void onClick(View v) {
-                money++;
-                tvGetMoney.setText("哈哈，我通过鼠标点击，轻易赚取了" + money + "元");
+
+                String strInputMoney = etGoaldMoney.getText().toString().trim();
+                int iMoney = Integer.parseInt(strInputMoney);
+
+                if(iMoney == money){
+                    Toast.makeText(MainActivity.this, "你经过努力已经完成目标", Toast.LENGTH_SHORT).show();
+                }else{
+                    money++;
+                    tvGetMoney.setText("哈哈，我通过鼠标点击，轻易赚取了" + money + "元");
+                }
+
             }
         });
 
